7e59a5e9e7e573b6607fcdb8dc72a908b130d389
Nao Komura <naoki.kom1270@gmail.com>
2020-08-14 20:50:40 +0900

    データサイエンス更新

--- R_pra.R
+++ R_pra.R
@@ -56,6 +56,56 @@ z<-getLogReturns(6)
 print(z)
 
 
+d<-function(x){
+  x<-x^2+log(x)
+ return(x)
+}
+d(3)
 
 
+ref<-getXReturn(10)
+ref
+(1/2)^10
+
+par(mfcol=c(1,1))
+
+n<-500
+q<-dbinom(n, 1000, 0.5)
+hist(q)
+geom_bar(mapping = aes(x=q,y=n))
+getwd()
+install.packages("sn")
+library("sn")
+xx=-30:30/10
+plot(xx,dsn(xx,0,1,0),type="l",ylim=c(0,0.8))
+lines(xx,dsn(xx,0,1,-2),lty=2)
+lines(xx,dsn(xx,0,1,2),lty=3)
+xx=-30:30/10
+plot(xx,dsn(xx,0,1,0),type="l",ylim=c(0,0.8))
+lines(xx,dsn(xx,0,1,-2),lty=2)
+lines(xx,dsn(xx,0,1,2),lty=3)
+source("/Users/naoki/Desktop/stock.d.R",encoding="cp932")
+dsn2=function(x){sum(log(dsn(xxx,xi=x[1],omega=x[2],alpha=x[3])))}
+xxx=stock.d[[2]]
+op.result=optim(c(0,1,1),dsn2,control=(fnscale=-1))
+op.result
+
+xx2=-20:20/10
+plot(xx2,dsn(xx2,op.result$par[1],op.result$par[2],op.result$par[3]),type="l")
+par(family= "HiraKakuProN-W3")
+
+stock.d.n=data.frame(stock.d$Date,scale(stock.d[,-1]))
+stock.dist=dist(t(stock.d.n[,-c(1,5)]))
+stock.dist
+stock.clust=hclust(stock.dist)
+plot(stock.clust)
+stock.d2=stock.d[,-c(1,3)]-stock.d[,3]
+lm.list=lapply(stock.d2[-1],function(x){lm(x~stock.d2[[1]])})
+lm.df=t(data.frame(lapply(lm.list,function(x){x$coef})))
+plot(hclust(dist(lm.df)))
+
+Ito.d<-read.csv("/Users/naoki/Desktop/R/R/Ito.csv", fileEncoding = "Shift_JIS")
+library(rpart)
+data("kyphosis")
+head(kyphosis)
 
